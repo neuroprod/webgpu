8686b6346006f9ce481659cdc5fa386399fccd45
kris temmerman <kris@neuroproductions.be>
2024-02-02 14:22:10 +0100

    fix

--- frontend/src/transitions/TextInfo.ts
+++ frontend/src/transitions/TextInfo.ts
@@ -7,7 +7,7 @@ export default class TextInfo extends Transition{
 
     set(onComplete: () => void,data:string=""){
         super.set(onComplete)
-        GameModel.lastClickObject =data;
+ 
         if(GameModel.textHandler.showHitTrigger(data)){
             GameModel.gameUI.cursor.show(CURSOR.NEXT)
         }else{
